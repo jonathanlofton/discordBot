28c90a43ba80509beda2d5558ae164467e61a402
Jonathan Lofton <jonathanlofton91@gmail.com>
2020-10-31 12:43:47 -0700

    remove redundant play

--- bot.py
+++ bot.py
@@ -15,18 +15,18 @@ async def on_message(message):
     if message.author.display_name == 'VAbreezy':
         await message.channel.send('well well well... look who decided to bless us with their presence today...')
 
-    if message.content.startswith('@here'):
+    if message.content.find('@here') != -1:
         await message.channel.send('hey is there anyone who wants to play?')
         await message.channel.send(file=discord.File('./photos/anyone_wanna_play.jpg'))
         await message.channel.send('p-p-p-please?')
+
+    if message.content.startswith('spooky'):
+        await message.channel.send('Happy Halloween! MUAHAHAHA!')
+        await message.channel.send(file=discord.File('./photos/spooky_smile.jpg'))
 
     if message.content.find('play') != -1:
         await message.channel.send('oh you want to play?')
         await message.channel.send(file=discord.File('./photos/mud_boys.jpg'))
         await message.channel.send('LETS PLAY')
-
-    if message.content.startswith('spooky'):
-        await message.channel.send('Happy Halloween! MUAHAHAHA!')
-        await message.channel.send(file=discord.File('./photos/spooky_smile.jpg'))
 
 client.run(config.BOT_TOKEN)
