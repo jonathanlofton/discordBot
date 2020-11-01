4a6842172ddeb74699864e70351af01f5b351723
Jonathan Lofton <jonathanlofton91@gmail.com>
2020-10-31 17:23:53 -0700

    more edits

--- bot.py
+++ bot.py
@@ -1,7 +1,29 @@
 import discord
+import emoji
 import config
 
 client = discord.Client()
+
+# all current users id's
+channel_users = config.channel_users
+# raw ids which will match the message request
+nick_author_id = channel_users['Nick']['author_id']
+jon_author_id = channel_users['Jon']['author_id']
+austin_author_id = channel_users['Austin']['author_id']
+frank_author_id = channel_users['Frank']['author_id']
+# notifies users
+nick_tag = f'<@{str(nick_author_id)}>'
+jon_tag = f'<@{str(jon_author_id)}>'
+austin_tag = f'<@{str(austin_author_id)}>'
+frank_tag = f'<@{str(frank_author_id)}>'
+
+# emojis in scope...
+eye_emoji = emoji.emojize(':eyes:')
+gun_emoji = emoji.emojize(':gun:')
+muscle_emoji = emoji.emojize(':muscle:')
+soccer_emoji = emoji.emojize(':soccer:')
+demon_emoji = emoji.emojize(':smiling_face_with_horns:')
+sparkle_emoji = emoji.emojize(':sparkles:')
 
 @client.event # our connection to discord, this interacts with the Discord WebSocket and API
 async def on_ready():
@@ -9,24 +31,75 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    author_id = message.author.id
+    name_of_messager = message.author.name
+    name_of_channel = message.channel.name
+    channel = message.channel
+    message_content = message.content.lower()
+
     if message.author == client.user:
         return
 
-    if message.author.display_name == 'VAbreezy':
-        await message.channel.send('well well well... look who decided to bless us with their presence today...')
+    if name_of_messager == 'VAbreezy':
+        await channel.send('well well well... look who decided to bless us with their presence today...')
 
-    if message.content.find('@here') != -1:
-        await message.channel.send('hey is there anyone who wants to play?')
-        await message.channel.send(file=discord.File('./photos/anyone_wanna_play.jpg'))
-        await message.channel.send('p-p-p-please?')
+    if message_content.find('@here') != -1:
+        await channel.send('hey is there anyone who wants to play?')
+        await channel.send(file=discord.File('./photos/anyone_wanna_play.jpg'))
+        await channel.send('p-p-p-please?')
 
-    if message.content.startswith('spooky'):
-        await message.channel.send('Happy Halloween! MUAHAHAHA!')
-        await message.channel.send(file=discord.File('./photos/spooky_smile.jpg'))
+    if message_content.startswith('@spooky'):
+        await channel.send(f'**Happy Halloween! {demon_emoji} MUAHAHAHA!**')
+        await channel.send(file=discord.File('./photos/spooky_smile.jpg'))
 
-    if message.content.find('play') != -1:
-        await message.channel.send('oh you want to play?')
-        await message.channel.send(file=discord.File('./photos/mud_boys.jpg'))
-        await message.channel.send('LETS PLAY')
+    if message_content.find('@playtime') != -1:
+        await channel.send('oh you want play time?')
+        await channel.send(file=discord.File('./photos/mud_boys.jpg'))
+        await channel.send('**LETS PLAY**')
+
+    if message_content.find('@val') != -1:
+
+        if author_id == nick_author_id:
+            await channel.send(f'{jon_tag} please carry me, I _can\'t_ get out of plat for the life of me.')
+
+        if author_id == jon_author_id:
+            # tag nick _____ times :)
+            huge_nick_tag, x = '', 0
+            while x < 5: 
+                huge_nick_tag += f'{nick_tag}\n'
+                x += 1
+
+            await channel.send(f'{huge_nick_tag} \n **GET THE FUCK ON DUDE**')
+
+        else:
+            await channel.send(f'oh shit, {name_of_messager} is stepping up... about god damn time... legooooo {nick_tag} {jon_tag}')
+
+    if message_content.find('@rl') != -1:
+
+        await channel.send(f'{jon_tag}{nick_tag}{austin_tag}{frank_tag} lets chase some balls...')
+
+    if message_content.find('!commands') != -1:
+        list_of_commands = """
+        {0}{0}**current commands:**{0}{0}
+        **@rl:** notifies ball chasers {1}
+        **@val:** notifies studs in the channel {2}{3}
+        **@playtime:** you asked for it {4} 
+        **@spooky:** {5} 
+        """
+        
+
+        await channel.send(list_of_commands.format(sparkle_emoji, soccer_emoji, gun_emoji, muscle_emoji, demon_emoji, eye_emoji))
+
+
+@client.event
+async def on_message_delete(message):
+    name_of_messager = message.author.name
+    await message.channel.send(f'{name_of_messager} why did you just delete: \n\n {message.content}... But, I\'ve been thinking a lot about these two...')
+    await message.channel.send(file=discord.File('./photos/mud_boys.jpg'))
+
+@client.event
+async def on_message_edit(before_message, after_message):
+    name = before_message.author.name
+    await before_message.channel.send(f'{name} what are you editing? ' + emoji.emojize(':thinking_face:'))
 
 client.run(config.BOT_TOKEN)
